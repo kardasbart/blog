08ff4113e4b909317bf654c1f99ac30d3f0363d8
Bartosz Kardas <kardasbart@gmail.com>
2021-12-02 22:02:57 +0100

    Added footer and margins

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
       <component :is="Component"></component>
     </transition>
   </router-view>
+  <Footer />
 </template>
 
 <script lang="ts">
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 export default {
-  components: { Navbar },
+  components: { Navbar, Footer },
   setup() {},
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="border-top border-primary mt-5">
+    <h6 class="text-center m-3">
+      Copyright © 2021 Bartosz Kardas, All&nbsp;Rights&nbsp;Reserved
+    </h6>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
